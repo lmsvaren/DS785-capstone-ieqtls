a9d52c6fcfdb7d97b37f09cbebdd941d11515ce1
lmsvaren <149616682+lmsvaren@users.noreply.github.com>
2024-08-01 09:41:07 -0400

    Add files via upload
    Add BayesPrism scripts and tensorqtl code

---
+++ 5.BayesPrism_macula.R
@@ -0,0 +1,116 @@
+# This script is adapted from the BayesPrism tutorial on GitHub
+# https://github.com/Danko-Lab/BayesPrism/tree/main
+# Purspose: Run BayesPrism
+
+##### WHAT TO CHANGE #####
+# Any of the inputs
+# sink() file name
+# bp.res, theta.cv, cellTypeFractions file names
+# Set GEP/count.matrix in 2 places
+
+##### PATHS #####
+# Variables
+input.type = "count.matrix"       #count.matrix or GEP
+gene.group = c("Rb","Mrp","other_Rb","chrM","MALAT1","chrY", "chrX")
+gene.type = c("protein_coding", "lincRNA")      # c("protein_coding", "pseudogene", "lincRNA")
+
+# Files
+dir = "/data/Segre_Lab/users/lsvaren/"
+file = "macula_big.class_071424"
+
+rdata.file = paste0(dir, file,".rdata")
+frac.file = paste0(dir, "cellTypeFractions_", file, ".csv")
+cv.file = paste0(dir, "thetaCV_", file, ".csv")
+
+# Start writing to file
+sink(file="macula_big.class_071424.txt")
+
+# Load BayesPrism
+library(BayesPrism)
+
+
+##### INPUTS #####
+## Bulk data ##
+# Rows = genes; Columns = sampleIDs
+bk.dat <- read.csv("bulk_macular_subset_counts.tsv", sep="\t", row.names=1)
+
+## Single cell reference data ##
+# Rows = bulk cell IDs; Columns = gene names or IDs
+sc.dat <- read.csv("downsample5k_macula_071124.csv", row.names=1)
+
+## Cell type labels ##
+# Should be the same length as nrow(sc.dat)
+meta = read.csv("meta_downsample5k_macula_071124.csv", row.names=1)
+cell.type.labels <- meta$big.class
+
+
+## Cell state labels ##
+# Same length as nrow(sc.dat)
+cell.state.labels <- cell.type.labels
+
+
+# DEFAULTS KEPT
+##### FILTER #####
+# Filter genes based on gene.group and number cells expressed
+sc.dat.filtered = cleanup.genes(input = sc.dat,
+                                input.type = input.type,
+                                species = "hs",
+                                gene.group = gene.group,
+                                exp.cells = 50) 
+
+
+# Subset based on gene type
+sc.dat.filtered.pc <- select.gene.type(sc.dat.filtered, 
+                                       gene.type = gene.type) 
+
+
+##### SELECT MARKER GENES #####
+# Pairwise t test for cell states from different cell types
+diff.exp.stat <- get.exp.stat(sc.dat=sc.dat[,colSums(sc.dat>0)>3],
+                              cell.type.labels=cell.type.labels,
+                              cell.state.labels=cell.state.labels,
+                              pseudo.count=0.1,
+                              cell.count.cutoff=3, 
+                              n.cores=1) 
+
+
+# Subset count matrix over signature genes
+sc.dat.filtered.pc.sig <- select.marker (sc.dat=sc.dat.filtered.pc,
+                                         stat=diff.exp.stat,
+                                         pval.max=0.01, 
+                                         lfc.min=0.1) 
+
+
+##### MAKE PRISM OBJECT #####
+myPrism <- new.prism(reference = sc.dat.filtered.pc.sig, 
+                     mixture = bk.dat,
+                     input.type = input.type, 
+                     cell.type.labels = cell.type.labels, 
+                     cell.state.labels = cell.state.labels, 
+                     key = NULL, 
+                     outlier.cut = 0.01, 
+                     outlier.fraction = 0.1) 
+
+
+
+##### RUN PRISM ####
+bp.res <- run.prism(prism = myPrism, n.cores = 50)
+
+#save
+save(bp.res, file=rdata.file)
+
+##### EXTRACT FINAL FRACTIONS AND CV'S #####
+theta <- get.fraction(bp = bp.res,
+                      which.theta = "final",
+                      state.or.type = "type")
+
+theta.cv <- bp.res@posterior.theta_f@theta.cv
+
+#Save
+write.csv(theta, frac.file)
+write.csv(theta.cv, cv.file)
+
+##### End file writing #####
+sink()
+
+
---
+++ 6.tensorqtl_macula.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import torch
+import tensorqtl
+from tensorqtl import pgen, cis, trans, post
+
+# Code adapted from tutorial at https://github.com/broadinstitute/tensorqtl
+# Run code in command line python3.9
+# Use virtual machine with GPU
+# Change cell_type and prefix until iterated through all cell types
+
+# Macula covariate and phenotype files
+plink_prefix_path = 'files/final_403_SHAPEIT_phased_IMPUTE2_imputed_filtered_variants_no_X'
+expression_bed = 'files/macular_batch1.rnaseqc_tmm.expression.bed.gz'
+covariates_file = 'files/macular_batch_1_eqtl_25_peers_10_genot_PCs_other_covariates.txt'
+
+# File with name to ID information
+merged = pd.read_csv('files/ids.csv', index_col=0)
+
+# load phenotypes and covariates: 21324 rows x 403 cols; 403 rows x 28 cols
+phenotype_df, phenotype_pos_df = tensorqtl.read_phenotype_bed(expression_bed)
+covariates_df = pd.read_csv(covariates_file, sep='\t', index_col=0).T
+
+# PLINK reader for genotypes 7057211 rows X 403 cols
+pgr = pgen.PgenReader(plink_prefix_path)
+genotype_df = pgr.load_genotypes()
+variant_df = pgr.variant_df
+
+# Make columns same using 
+geno_cols = pd.DataFrame(genotype_df.columns.tolist(), columns=['cols'])
+maps = geno_cols.merge(merged, left_on='cols', right_on='genotype_id')
+genotype_df.columns = maps.X.tolist()
+
+# Change variant_df['chrom'] to match phenotype_pos_df['chr'] format
+variant_df['chrom'] = variant_df['chrom'].apply(lambda x: f'chr{x}')
+
+# Sort indexes to make sure they match in the two dataframes
+covariates_df = covariates_df.sort_index(axis=0)
+phenotype_df = phenotype_df.sort_index(axis=1)
+
+
+
+######### RUN CODE BELOW FOR EACH CELL TYPE #########
+# ['RGC', 'AC', 'Rods', 'Cones', 'BC-OFF', 'MG', 'HC', 'BC-ON', 'Astro']
+cell_type = 'RGC'
+prefix = 'rgc_macula_0.1'
+maf = 0.1
+
+# Load interaction dataframe
+interaction_df = pd.read_csv('files/cellTypeFractions_macula_big.class_071424.csv', index_col=0)
+
+# load interaction_df
+interaction_df = interaction_df[[cell_type]]
+
+# Remove X at the beginning of each index name
+interaction_df.index = interaction_df.index.str.lstrip('X')
+
+# run file
+cis.map_nominal(genotype_df, variant_df, phenotype_df, phenotype_pos_df, prefix,
+                covariates_df=covariates_df,
+                interaction_df=interaction_df, maf_threshold_interaction=maf, #default is 0.05
+                run_eigenmt=True, output_dir='.', write_top=True, write_stats=True)
